4d2cd12aa152fe6983e2f949872984c3e9b1926b
Walodja1987 <wladimir.weinbender@gmail.com>
2023-07-10 17:10:10 +0200

    TransferERC20 hook added

--- components/TransferERC20.tsx
+++ components/TransferERC20.tsx
@@ -2,13 +2,22 @@ import * as React from 'react'
 import {
   usePrepareContractWrite,
   useContractWrite,
-  useContractRead
+  useContractRead,
+  useWaitForTransaction,
+  useNetwork,
+  useAccount,
 } from 'wagmi'
 import { parseUnits } from 'viem'
 import erc20ABI from '../abis/IERC20.json'
-import { useDebounce } from './useDebounce'
+import { useDebounce } from 'usehooks-ts' // Alternatively, check useDebounce.tsx component, suggested here: https://wagmi.sh/examples/contract-write-dynamic#step-5-add-a-debounce-to-the-input-value 
  
 export function TransferERC20() {
+  // Get connected wallete address
+  const { address } = useAccount();
+  
+  // Get the info about the connected chain (incl. name, id, native currency, block explorer link, etc.)
+  const { chain } = useNetwork();
+
   const [recipient, setRecipient] = React.useState('')
   const [amount, setAmount] = React.useState('')
 
@@ -16,29 +25,45 @@ export function TransferERC20() {
   // and on every change to `args`, we don't want to spam the RPC and become rate-limited.
   // To mitigate this, delay the update of a value after 500 milliseconds of inactivity
   const debouncedRecipient = useDebounce(recipient, 500)
-  const debouncedAmount = useDebounce(amount, 500)
+  const debouncedAmount = useDebounce(amount, 500) as `${number}` // Field input is restricted to number. Hence, cast to `${number}`
+
+  const erc20TokenAddress = '0xf5d5Ea0a5E86C543bEC01a9e4f513525365a86fD' // dUSD on Mumbai
   
-  const { data } = useContractRead({
-    address: '0xBE53A7e28be039259a2f19eA0A766756d2c5A0EE',
+  const { data: readData } = useContractRead({
+    address: erc20TokenAddress,
     abi: erc20ABI,
     functionName: 'decimals',
   })
-  const decimals = data as number
-
-  // @todo continue in step 7: https://wagmi.sh/examples/contract-write-dynamic#step-5-add-a-debounce-to-the-input-value
+  const decimals = readData as number
 
   // Give back a "prepared config" to be sent through to, including gas estimate and other information
-  // needed for the transaction
-  const { config, error } = usePrepareContractWrite({
-    address: '0xBE53A7e28be039259a2f19eA0A766756d2c5A0EE',
+  // needed for the transaction. The function also also performs basic checks such as address validity and whether the  
+  // provided token address is indeed a contract.
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError
+  } = usePrepareContractWrite({
+    address: erc20TokenAddress,
     abi: erc20ABI,
     functionName: 'transfer',
-    args: [debouncedRecipient, parseUnits(debouncedAmount || '0', decimals)], // || '0' required to handle potentially undefined `debouncedAmount` value
+    args: [
+      debouncedRecipient,
+      parseUnits(debouncedAmount || '0', decimals)
+    ], // || '0' required to handle potentially undefined `debouncedAmount` value
     enabled: Boolean(debouncedRecipient && debouncedAmount), // Run this query automatically only if both, recipient and amount are defined
   })
 
   // Perform the actual contract write transaction.
-  const { write } = useContractWrite(config)
+  const { data: writeData, error: writeError, isError: isWriteError, write } = useContractWrite(config)
+  console.log("writeData", writeData)
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: writeData?.hash,
+  })
+
+  // Get the block explorer link from the chains array based on the chain ID
+  const blockExplorerLink = chain?.blockExplorers?.default.url;
 
   // Render
   return (
@@ -48,28 +73,42 @@ export function TransferERC20() {
         write?.()
       }}
     >
-      <label htmlFor="Recipient">Recipient</label>
-      <input
-        id="recipient"
-        onChange={(e) => setRecipient(e.target.value)}
-        placeholder=""
-        value={recipient}
-        autoComplete="off"
+      <div>
+        <label htmlFor="Recipient">Recipient</label>
+        <input
+          id="recipient"
+          onChange={(e) => setRecipient(e.target.value)}
+          placeholder=""
+          value={recipient}
+          autoComplete="off"
         />
-      <label htmlFor="Amount">Amount</label>
-      <input
-        id="amount"
-        onChange={(e) => setAmount(e.target.value)}
-        placeholder=""
-        value={amount}
-        autoComplete="off"
-      />
+      </div>
+      <div>
+        <label htmlFor="Amount">Amount</label>
+        <input
+          id="amount"
+          type="number"
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder=""
+          value={amount}
+          autoComplete="off"
+        />
+      </div>
       <>
-        <button disabled={!write}>
-          Transfer
+        <button disabled={!write || isLoading}>          
+          {isLoading ? 'Transferring...' : 'Transfer'}
         </button>
-        {error && (
-          <div>An error occurred preparing the transaction: {error.message}</div>
+        {isSuccess && (
+          <div>
+            Successfully transferred the token!
+            <div>
+              <a href={`${blockExplorerLink}/tx/${writeData?.hash}`}>Block explorer</a>
+            </div>
+          </div>
+        )}
+        {/* Throw if there was an error while preparing the write contract tx */}
+        {(isPrepareError || isWriteError) && (
+          <div>Error: {(prepareError || writeError)?.message}</div>
         )}
       </>
     </form>
